c3e2cb1be0247bcd41f9090814680de69c8d1afd
Eric Masiello <emasiello@vistaprint.com>
2024-09-15 15:43:55 -0400

    Initial Commit

---
+++ Clock It/Clock_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_ItApp.swift
+//  Clock It
+//
+//  Created by Eric Masiello on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Clock_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
